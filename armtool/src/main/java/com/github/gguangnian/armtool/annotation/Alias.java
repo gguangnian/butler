e057be0fcf1e47047983ccf21553e01d7146fa28
guochengzhi <guochengzhi@test.com>
2022-06-22 20:25:29 +0800

    add some

---
+++ armtool/src/main/java/com/github/gguangnian/armtool/annotation/Alias.java
@@ -0,0 +1,12 @@
+package com.github.gguangnian.armtool.annotation;
+
+/**
+ * 别名注解，使用此注解的字段、方法、参数等会有一个别名
+ *
+ * @author gcz
+ * @Date 2022/6/22
+ */
+public @interface Alias {
+
+    String value();
+}
--- armtool/src/main/java/com/github/gguangnian/armtool/util/ReflectUtil.java
+++ armtool/src/main/java/com/github/gguangnian/armtool/util/ReflectUtil.java
@@ -1,8 +1,24 @@
 package com.github.gguangnian.armtool.util;
 
+import java.lang.reflect.AccessibleObject;
+
 /**
  * @author gcz
  * @Date 2022/6/22
  */
 public class ReflectUtil {
+
+    /**
+     * 设置方法为可访问（私有方法可以被外部调用）
+     *
+     * @param <T>              AccessibleObject的子类，比如Class、Method、Field等
+     * @param accessibleObject 可设置访问权限的对象，比如Class、Method、Field等
+     * @return 被设置可访问的对象
+     */
+    public static <T extends AccessibleObject> T setAccessible(T accessibleObject) {
+        if (accessibleObject != null && accessibleObject.isAccessible() == false) {
+            accessibleObject.setAccessible(true);
+        }
+        return accessibleObject;
+    }
 }
