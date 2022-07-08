f0c4bc09cb6666cf6d323f47c57d2d4aa6a0e426
guochengzhi <guochengzhi@test.com>
2022-07-08 13:57:59 +0800

    add some

--- armtool/src/main/java/com/github/gguangnian/armtool/tree/TreeUtil.java
+++ armtool/src/main/java/com/github/gguangnian/armtool/tree/TreeUtil.java
@@ -1,6 +1,7 @@
 package com.github.gguangnian.armtool.tree;
 
 import com.github.gguangnian.armtool.test.Assert;
+import com.github.gguangnian.armtool.tree.parser.DefaultNodeParser;
 import com.github.gguangnian.armtool.tree.parser.NodeParser;
 import com.github.gguangnian.armtool.util.CollUtil;
 
@@ -17,6 +18,31 @@ public class TreeUtil {
     /**
      * 树构建
      *
+     * @param <E>    ID类型
+     * @param list   源数据集合
+     * @param rootId 最顶层父id值 一般为 0 之类
+     * @return List
+     */
+    public static <E> List<Tree<E>> build(List<TreeNode<E>> list, E rootId) {
+        return build(list, rootId, TreeNodeConfig.DEFAULT_CONFIG);
+    }
+
+    /**
+     * 树构建
+     *
+     * @param <E>            ID类型
+     * @param list           源数据集合
+     * @param rootId         最顶层父id值 一般为 0 之类
+     * @param treeNodeConfig 配置
+     * @return List
+     */
+    public static <E> List<Tree<E>> build(List<TreeNode<E>> list, E rootId, TreeNodeConfig treeNodeConfig) {
+        return build(list, rootId, treeNodeConfig, new DefaultNodeParser<E>());
+    }
+
+    /**
+     * 树构建
+     *
      * @param <T>            转换的实体 为数据源里的对象类型
      * @param <E>            ID类型
      * @param list           源数据集合
@@ -30,6 +56,7 @@ public class TreeUtil {
             return new ArrayList<Tree<E>>(0);
         }
         Assert.isNotNull(rootId);
+        Assert.isNotNull(nodeParser);
 
         List<Tree<E>> treeList = new ArrayList<>(list.size());
         for (T obj : list
